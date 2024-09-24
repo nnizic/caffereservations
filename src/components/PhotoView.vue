2890377383cdd779a781f759724b7053c1c5e698
giz73 <nneven@proton.me>
2024-09-24 21:29:23 +0200

    weather report

--- src/components/PhotoView.vue
+++ src/components/PhotoView.vue
@@ -20,6 +20,38 @@ export default {
       return moment(this.info.time).locale("hr").fromNow();
     },
   },
+  methods: {
+    async fetchWeatherData() {
+      try {
+        const response = await fetch(
+          "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Zadar?unitGroup=metric&key=ZQVJ86FZTZXZVSWNPT6CHURBH&contentType=json",
+          {
+            method: "GET",
+            headers: { accept: "application/json" },
+          },
+        );
+
+        const data = await response.json();
+
+        // Map over the days and return the relevant information
+        return data.days.map((day) => ({
+          icon: day.icon,
+          datetime: day.datetime,
+          temperature: day.temp,
+          description: day.description,
+        }));
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
+  mounted() {
+    this.fetchWeatherData().then((weatherData) => {
+      weatherData.forEach((day) => {
+        console.log(day.description);
+      });
+    });
+  },
 };
 </script>
 <style lang="scss"></style>
