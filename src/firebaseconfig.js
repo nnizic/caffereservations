6fbb53a2d65386668827cb95db66479430abe98a
giz73 <nneven@proton.me>
2024-10-31 14:56:43 +0100

    firebase_config

---
+++ src/firebaseconfig.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYHgV9cpn1l6RSMqrwgvmli0QU-lo68mg",
+
+  authDomain: "caffebaret-as11a.firebaseapp.com",
+
+  projectId: "caffebaret-as11a",
+
+  storageBucket: "caffebaret-as11a.appspot.com",
+
+  messagingSenderId: "48282034903",
+
+  appId: "1:48282034903:web:05b5b16238902861630692",
+};
+
+export default { firebaseConfig };
